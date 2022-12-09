1a55f9f0700c3d09002f9d2c6c73d38f8767957e
Bagolly <>
2022-12-09 21:12:47 +0100

    Day 2. Excercise 11 - 13 DONE.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    let today = new Date();
 
+    let oldest = people.reduce((max, current) => {
+
+        let maxAge = max.hasOwnProperty("yearOfDeath") ? 
+            max.yearOfDeath - max.yearOfBirth : 
+            today.getFullYear() - max.yearOfBirth;
+        
+        let currentAge = current.hasOwnProperty("yearOfDeath") ? 
+            current.yearOfDeath - current.yearOfBirth : 
+            today.getFullYear() - current.yearOfBirth;
+
+
+        return (currentAge > maxAge) ? current : max;
+    });
+
+    return oldest;
 };
 
 // Do not edit below this line
--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,6 +1,12 @@
-const caesar = function() {
+const caesar = (txt, shift) => 
+    txt.split("")
+    .map(c => encodeByte(c, shift, (c === c.toLowerCase() ? 97 : 65), 26))
+    .join("");
 
-};
+
+const encodeByte = (c, shift, min, range) =>
+    c.toLowerCase() === c.toUpperCase() ? c :
+    String.fromCodePoint((((c.codePointAt(0) + shift - min) % range) + range) % range + min);
 
 // Do not edit below this line
 module.exports = caesar;
